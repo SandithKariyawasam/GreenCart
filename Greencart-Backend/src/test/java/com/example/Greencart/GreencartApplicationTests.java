f9ca4d24e297ee5dc1463ec9ccb9d52a05832d7d
Sandith Kariyawasam <136833383+SandithKariyawasam@users.noreply.github.com>
2025-12-12 01:19:43 +0530

    Create basic project

---
+++ Greencart-Backend/src/test/java/com/example/Greencart/GreencartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Greencart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreencartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
